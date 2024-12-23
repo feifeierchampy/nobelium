43d570b6db81dc4a588f6c22df8c0e242f208539
chmini <chmini@gmail.com>
2024-12-23 13:03:21 +0800

    feat: 导航栏调整

--- components/Header.js
+++ components/Header.js
@@ -11,10 +11,11 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true },
-    { id: 4, name: locale.NAV.TOOLS, to: 'tools.feifeier.com', show: true, external: true },
-    { id: 5, name: locale.NAV.APPS, to: 'www.freoza.com', show: true, external: true}
+    { id: 2, name: locale.NAV.TOOLS, to: 'https://tools.feifeier.com', show: true, external: true },
+    { id: 3, name: locale.NAV.APPS, to: 'https://www.freoza.com', show: true, external: true},
+    { id: 4, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
+    { id: 5, name: locale.NAV.SEARCH, to: '/search', show: true }
+
   ]
   return (
     <div className="flex-shrink-0">
