89493bdc6c847e8a0987d514590d5c4ceadff452
chmini <chmini@gmail.com>
2024-12-23 23:38:03 +0800

    feat: 调整导航栏顺序

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,11 @@ const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.TOOLS, to: 'https://tools.feifeier.com', show: true, external: true },
-    { id: 3, name: locale.NAV.APPS, to: 'https://www.freoza.com', show: true, external: true},
+    { id: 1, name: locale.NAV.TOOLS, to: 'https://tools.feifeier.com', show: true, external: true },
+    { id: 2, name: locale.NAV.APPS, to: 'https://www.freoza.com', show: true, external: true},
+    { id: 3, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 4, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
     { id: 5, name: locale.NAV.SEARCH, to: '/search', show: true }
-
   ]
   return (
     <div className="flex-shrink-0">
