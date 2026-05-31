3be790ffaf9f86b9087c1400ea1639ac5baf01d1
cham <332560026@qq.com>
2026-05-31 23:13:54 +0800

    fix: change site

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'cham\'s blog',
   author: 'cham',
   email: 'cham@feifeier.com',
-  link: 'https://nobelium-theta-six.vercel.app/',
+  link: 'https://feifeier.com',
   description: '',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
