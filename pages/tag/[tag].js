9590f8384049fc4745e97e03d3648c7898a9c6ea
cham <332560026@qq.com>
2026-05-31 22:47:54 +0800

    修复 Vercel 动态页面构建失败

--- pages/tag/[tag].js
+++ pages/tag/[tag].js
@@ -24,10 +24,8 @@ export async function getStaticProps ({ params }) {
 }
 
 export async function getStaticPaths () {
-  const posts = await getAllPosts({ includePages: false })
-  const tags = getAllTagsFromPosts(posts)
   return {
-    paths: Object.keys(tags).map(tag => ({ params: { tag } })),
-    fallback: true
+    paths: [],
+    fallback: 'blocking'
   }
 }
